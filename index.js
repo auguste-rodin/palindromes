600989a76caac3f40f1ba96a2782bbb96f360482
Dan Ogden <danogdenvo2@gmail.com>
2020-05-25 14:12:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+ // The task is to check whether a string is a palindrome or not, and return true if it is.
+// So what you need to do is reverse the string and compare the two with a boolean question.
+
+//One way is with an 'if' statement.
+
+/*
+function isPalindrome(str) {
+  const revString = str.split('').reverse().join('');
+
+return revString === str;
+}
+
+const output = isPalindrome('bob');
+
+console.log(output); */
+
+// a simple explanation using the in-built methods.
